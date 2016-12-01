8195b759efe5b41bb92cdb25c73cfc2e4d7c6717
anamklone <amk226@pitt.edu>
2016-12-01 16:27:42 -0500

    Remove debugging statements that print to the console

--- assignment4.js
+++ assignment4.js
@@ -21,14 +21,10 @@
   
   $(window).on('load', function() {
     $.getJSON("http://www.mattbowytz.com/simple_api.json?data=all", function(result) {
-      console.log(result);
       if (result.code == 8 && result.status == 200) {
         data = result.data;
-        console.log(data);
         interests = data.interests;
         programming = data.programming;
-        console.log(interests);
-        console.log(programming);
       }
     });
   });
@@ -36,18 +32,15 @@
   $('.flexsearch-input').on('keyup', function() {
     $('#flexsearch-popup').html('');
     var input = $('.flexsearch-input').val();
-    console.log(input);
     if (data != null && interests != null & programming != null) {
       if (input.length > 0) {
         for (i = 0; i < interests.length; i++) {
           if (interests[i].substring(0, input.length).toLowerCase() == input.toLowerCase()) {
-            console.log(interests[i]);
             $('#flexsearch-popup').append(interests[i] + '<br>');
           }
         }
         for (i = 0; i < programming.length; i++) {
           if (programming[i].substring(0, input.length).toLowerCase() == input.toLowerCase()) {
-            console.log(programming[i]);
             $('#flexsearch-popup').append(programming[i] + '<br>');
           }
         }
